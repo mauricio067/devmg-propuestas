b6d2423368daa27cb4b0b76c3dc45a2ba41997f8
Mauricio <mauricio067@gmail.com>
2014-10-16 18:19:00 -0300

    Cambio de ubicacion de archivos

--- public/js/main.js
+++ public/js/main.js
@@ -7,23 +7,23 @@
 	app.config(['$routeProvider', function($routeProvider) {
 
 		$routeProvider.when('/my-sitds', {
-			templateUrl: 'templates/list.html',
+			templateUrl: '../templates/list.html',
 			controller: 'SitdController'
 		});
 		$routeProvider.when('/', {
-			templateUrl: 'templates/add.html',
+			templateUrl: '../templates/add.html',
 			controller: 'SitdController'
 		});
 		$routeProvider.when('/add', {
-			templateUrl: 'templates/add.html',
+			templateUrl: '../templates/add.html',
 			controller: 'SitdController'
 		});
 		$routeProvider.when('/edit/:textId', {
-			templateUrl: 'templates/add.html',
+			templateUrl: '../templates/add.html',
 			controller: 'SitdEditController'
 		});
 		$routeProvider.when('/view/:textId', {
-			templateUrl: 'templates/view.html',
+			templateUrl: '../templates/view.html',
 			controller: 'SitdViewController'
 		});
 	}]);
